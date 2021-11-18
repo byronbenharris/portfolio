efdf036e9dde12c269a8b638e7f79fbeefef4fb2
byron-harris <bbh3@rice.edu>
2021-11-18 00:33:52 -0600

    adding anchors to home

--- pages/index.js
+++ pages/index.js
@@ -34,18 +34,23 @@ export default function Home({ allBlogData }) {
           alt="Ben Harris"
         />
         <h1 className={utilStyles.heading2Xl}>Ben Harris</h1>
+        <p></p>
+        
       </header>
 
+      <ul>
+        <li><a href="#Work">Work</a></li>
+        <li><a href="#Projects">Projects</a></li>
+        <li><a href="#Blog">Blog</a></li>
+        <li><a href="#Contact">Contact</a></li>
+      </ul>
+
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>About</h2>
+        <h2 id="Work" className={utilStyles.headingLg}>Work</h2>
       </section>
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Work</h2>
-      </section>
-
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Projects</h2>
+        <h2 id="Projects" className={utilStyles.headingLg}>Projects</h2>
         <div className={homeStyles.grid}>
 
           <a href="" className={homeStyles.card}>
@@ -53,31 +58,11 @@ export default function Home({ allBlogData }) {
             <p></p>
           </a>
 
-          <Link href="/docs">
-            <a className={homeStyles.card}>
-              <h2>Documentation</h2>
-              <p></p>
-            </a>
-          </Link>
-
-          <Link href="/list">
-            <a className={homeStyles.card}>
-              <h2>Explore</h2>
-              <p></p>
-            </a>
-          </Link>
-
-          <Link href="/upload">
-            <a className={homeStyles.card}>
-              <h2>Upload</h2>
-              <p></p>
-            </a>
-          </Link>
         </div>
       </section>
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <h2 id="Blog" className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
             {allBlogData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
@@ -94,7 +79,12 @@ export default function Home({ allBlogData }) {
       </section>
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Contact</h2>
+        <h2 id="Contact" className={utilStyles.headingLg}>Contact</h2>
+        <ul>
+          <li><a href="https://twitter.com/byronbenharris">Twitter</a></li>
+          <li><a href="https://github.com/byronbenharris">GitHub</a></li>
+          <li><a href="https://linkedin.com/in/byronbenharris">LinkedIn</a></li>
+        </ul>
       </section>
       
     </div>
